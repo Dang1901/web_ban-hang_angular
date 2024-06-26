897a9da23fb86e9c979543c173c5a824e087e577
DuyTran <ngocduytran000315@gmail.com>
2024-06-26 23:27:04 +0700

    update code

--- src/app/components/layout-admin/layout-admin.component.ts
+++ src/app/components/layout-admin/layout-admin.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from "../layouts/sidebar/sidebar.component";
+import { SidebarComponent } from '../layouts/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from "../layouts/header/header.component";
-import { DashboardComponent } from "../../view/dashboard/dashboard.component";
+import { HeaderComponent } from '../layouts/header/header.component';
+import { DashboardComponent } from '../../view/dashboard/dashboard.component';
 
 @Component({
-    selector: 'app-layout-admin',
-    standalone: true,
-    templateUrl: './layout-admin.component.html',
-    styleUrl: './layout-admin.component.css',
-    imports: [SidebarComponent, RouterModule, HeaderComponent, DashboardComponent]
+  selector: 'app-layout-admin',
+  standalone: true,
+  templateUrl: './layout-admin.component.html',
+  styleUrl: './layout-admin.component.scss',
+  imports: [
+    SidebarComponent,
+    RouterModule,
+    HeaderComponent,
+    DashboardComponent,
+  ],
 })
 export class LayoutAdminComponent {
-
+  activeIndex: number = 0;
+  ngOnInit(): void {}
+  toggleActive(index: number) {
+    this.activeIndex = index;
+  }
 }
