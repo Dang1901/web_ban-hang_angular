b23e3fff111c5f8bb51eace610fe661c11302d6a
haidang321091 <phamhaidang190123@gmail.com>
2024-06-28 23:18:39 +0700

    update code 28/6

--- src/app/components/layout-client/layout-client.component.ts
+++ src/app/components/layout-client/layout-client.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-layout-client',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './layout-client.component.html',
   styleUrl: './layout-client.component.css'
 })
