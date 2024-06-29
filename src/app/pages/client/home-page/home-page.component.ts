1f2aa0d28114fd3e377182d0e848f2186920b23a
DuyTran <ngocduytran000315@gmail.com>
2024-06-30 00:15:26 +0700

    update

--- src/app/pages/client/home-page/home-page.component.ts
+++ src/app/pages/client/home-page/home-page.component.ts
@@ -5,6 +5,7 @@ import { IProduct } from '../../../interfaces/Product';
 import { ProductService } from '../../../service/product.service';
 import { BannerComponent } from '../../../components/layouts/banner/banner.component';
 import { CartService } from '../../../service/cart.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-home-page',
@@ -15,26 +16,47 @@ import { CartService } from '../../../service/cart.service';
 })
 export class HomePageComponent implements OnInit {
   products: IProduct[] | undefined;
-
+  userInfo: any = {} as any;
   constructor(
     private productService: ProductService,
     private cartService: CartService,
-    private router: Router
+    private router: Router,
+    private cookieService: CookieService
   ) {}
   ngOnInit() {
     this.productService.getProducts().subscribe((products) => {
       this.products = products;
     });
+    this.userInfo = this.getUserInfoFromCookie();
   }
   addToCart(id: any) {
-    if (id) {
-      this.cartService.addItem(id, 1).subscribe(() => {
-        alert('Sản phẩm đã được thêm vào giỏ hàng!');
-        if (confirm('Bạn có muốn thanh toán luôn không!')) {
-          this.router.navigate(['/cart']);
-        }
-      });
+    if (this.userInfo) {
+      if (id) {
+        this.cartService.addItem(id, 1).subscribe(() => {
+          alert('Sản phẩm đã được thêm vào giỏ hàng!');
+          if (confirm('Bạn có muốn thanh toán luôn không!')) {
+            this.router.navigate(['/cart']);
+          }
+        });
+      }
+      return;
     }
+    if (!this.userInfo) {
+      alert('Bạn cần đăng nhập để mua hàng!');
+      this.router.navigate(['/login']);
+    }
+  }
+  getUserInfoFromCookie() {
+    const userInfoString = this.cookieService.get('userInfo');
+    if (userInfoString) {
+      try {
+        return JSON.parse(userInfoString);
+      } catch (error) {
+        console.error('Error parsing userInfo from cookie:', error);
+        return null;
+      }
+    }
+    return null;
   }
   formatCurrency(amount: number): string {
     const formatter = new Intl.NumberFormat('vi-VN', {
