49a148a230ad97cca074a0b916f3c177f022d994
Hoang Cao Huan (FPL HN) <huanhcph35218@fpt.edu.vn>
2024-06-29 15:21:20 +0700

    cart

--- src/app/components/layouts/header/header.component.ts
+++ src/app/components/layouts/header/header.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -14,14 +15,30 @@ export class HeaderComponent implements OnInit {
   searchForm = new FormGroup({
     keywords : new FormControl('')
   });
-  router= new Router()
-  ngOnInit(): void {
-      
+  totalItems: number = 0;
+  constructor(
+    private cartService: CartService,
+    private router: Router
+  ) {}
+ ngOnInit(): void {
+    this.loadCartItems();
+    this.cartService.getCartUpdated().subscribe(() => {
+      this.loadCartItems();
+    });
+  }
+  loadCartItems(): void {
+    this.cartService.getItems().subscribe((items) => {
+      this.totalItems = items.reduce((sum, item) => sum + item.quantity, 0);
+    });
   }
   onSearch(){
     const keywords = this.searchForm.controls.keywords.value
       this.router.navigate(['search'],{
         queryParams: {keywords: keywords}
       })
+  }
+
+  cart(): void {
+    this.router.navigate(['/cart']);
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { ICart } from '../interfaces/Cart';
 
 @Injectable({
@@ -9,19 +9,25 @@ import { ICart } from '../interfaces/Cart';
 export class CartService {
   private baseUrl = 'http://localhost:3000/cart';
   private base = 'http://localhost:3000/cart?_expand=product';
+  private cartUpdated = new Subject<void>();
 
   constructor(private http: HttpClient) {}
+  
 
   getItems(): Observable<ICart[]> {
     return this.http.get<ICart[]>(this.base);
   }
 
   addItem(productId: number, quantity: number): Observable<ICart> {
-    return this.http.post<ICart>(this.baseUrl, { productId, quantity });
+    return this.http.post<ICart>(this.baseUrl, { productId, quantity }).pipe(
+      tap(() => this.cartUpdated.next()) // Phát sự kiện khi thêm sản phẩm vào giỏ hàng
+    );
   }
 
   removeItem(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+    return this.http.delete<void>(`${this.baseUrl}/${id}`).pipe(
+      tap(() => this.cartUpdated.next()) // Phát sự kiện khi thêm sản phẩm vào giỏ hàng
+    );
   }
 
   updateCartItem(item: ICart): Observable<ICart> {
@@ -31,5 +37,8 @@ export class CartService {
 
   clearCart(): Observable<void> {
     return this.http.delete<void>(this.baseUrl);
+  }
+  getCartUpdated(): Observable<void> {
+    return this.cartUpdated.asObservable();
   }
 }
