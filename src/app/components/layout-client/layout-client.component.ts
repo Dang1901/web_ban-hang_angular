2ed2e59baf39ef94a47e0029bdc0979bf3493701
Hoang Cao Huan (FPL HN) <huanhcph35218@fpt.edu.vn>
2024-06-29 23:51:42 +0700

    Huan test

--- src/app/components/layout-client/layout-client.component.ts
+++ src/app/components/layout-client/layout-client.component.ts
@@ -2,13 +2,12 @@ import { FooterComponent } from './../layouts/footer/footer.component';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../layouts/header/header.component';
 import { RouterModule } from '@angular/router';
-import { BannerComponent } from '../layouts/banner/banner.component';
 
 @Component({
   selector: 'app-layout-client',
   standalone: true,
   templateUrl: './layout-client.component.html',
   styleUrl: './layout-client.component.css',
-  imports: [HeaderComponent, RouterModule, FooterComponent, BannerComponent],
+  imports: [HeaderComponent, RouterModule, FooterComponent],
 })
 export class LayoutClientComponent {}
